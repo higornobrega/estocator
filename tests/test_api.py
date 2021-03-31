f010fb72db5b718252a242f8a0741cfa1b0da97a
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-03-30 23:12:52 -0300

    criando o get_api_headers

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,3 +4,11 @@ import unittest
 class APITestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
+
+    def get_api_headers(self, username, password):
+        return {
+            'Authorization': 'Basic ' + b64encode(
+                (username + ':' + password).encode('utf-8')).decode('utf-8'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
