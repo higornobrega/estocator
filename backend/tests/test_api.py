a27c2bf40163778c91b74ad9526ce372f7103f1f
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-04-06 17:56:16 -0300

    Recriando os testes

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,24 +1,14 @@
+import json
 import unittest
 
-from app import app
+from backend import app
 
 
 class APITestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def get_api_headers(self, username, password):
-        return {
-            'Authorization': 'Basic ' + b64encode(
-                (username + ':' + password).encode('utf-8')).decode('utf-8'),
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-
-    def test_404(self):
-        response = self.client.get(
-            '/wrong/url',
-            headers=self.get_api_headers('email', 'password'))
-        self.assertEqual(response.status_code, 404)
-        json_response = json.loads(response.get_data(as_text=True))
-        self.assertEqual(json_response['error'], 'not found')
+    def test_home(self):
+        response = self.app.get("/")
+        self.assertEqual(json.loads(
+            response.data.decode("utf-8")), {"hello": "Flask!"})
