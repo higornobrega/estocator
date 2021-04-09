bf5e4cda55745865c385936798e3fb433ac2fd06
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-04-09 15:20:59 -0300

    Adicionando configurações do model

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'my_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
