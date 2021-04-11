9b4fe724b35e2655d14f2d1fb6dc13deb2eec043
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-04-11 12:52:19 -0300

    Protegendo as páginas

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 basedir = os.path.abspath(os.path.dirname(__file__))
--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
+
 from .models import User
 from . import db
-
-from flask_login import login_user
 
 auth = Blueprint('auth', __name__)
 
@@ -20,13 +19,10 @@ def login_post():
 
     user = User.query.filter_by(email=email).first()
 
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))
 
-    # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
 
@@ -42,17 +38,16 @@ def signup_post():
     endereco = request.form.get('endereco')
     cpf = request.form.get('cpf')
 
-    user_email = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
-    user_cpf = User.query.filter_by(cpf=cpf).first() # if this returns a user, then
+    user_email = User.query.filter_by(email=email).first()
+    user_cpf = User.query.filter_by(cpf=cpf).first()
 
-    if user_email: # if a user is found, we want to redirect back to signup page so user can try again
+    if user_email:
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
     elif user_cpf:
         flash('CPF address already exists')
         return redirect(url_for('auth.signup'))
 
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(
         email=email,
         name=name,
@@ -68,5 +63,7 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
