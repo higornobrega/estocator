6a2b25bcc097f52e35e45e4a71223024ab584fa8
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-04-14 11:16:49 -0300

    Adicionando models e migrations

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=50)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.IntegerField()
     bar_code = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.name + " - Bar code: " + str(self.bar_code)
