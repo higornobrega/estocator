8a52096b53c3d1bbfad34fa5d6fc2675a2892d5d
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-04-14 11:06:44 -0300

    Adicionando model de produtos

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2)
+    quantity = models.IntegerField()
+    bar_code = models.IntegerField(unique=True)
