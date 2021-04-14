0bad90c49421ad04eb5e3f56c6aeb96bbf5732a5
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-04-14 10:03:29 -0300

    Removendo projeto flask

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-from flask_login import UserMixin
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(150))
-    endereco = db.Column(db.String(200))
-    cpf = db.Column(db.String(11), unique=True)
