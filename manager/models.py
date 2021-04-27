68d0eb313b7e076a10aa7722b1315e5be6b16b9d
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-04-27 18:19:28 -0300

    Update dos models

--- manager/models.py
+++ manager/models.py
@@ -6,7 +6,7 @@ class Maneger(models.Model):
     address = models.CharField(max_length=100)
     birthday = models.DateField()
     email = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
+    cpf = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.name
